65ac7783be403716fbb4e44356489c2e452102f6
tsukolsky <tsukolsky@sigovs.com>
2013-04-26 11:19:48 -0400

    Finished first prototyping for receive uart from the Rasberry Pi. Needs to now be field tested. New Branch

--- AVR_CODE/theWeather.system_board_324P/theWeather.system_board_3244/main.c
+++ AVR_CODE/theWeather.system_board_324P/theWeather.system_board_3244/main.c
@@ -3,7 +3,7 @@
 | Author: Todd Sukolsky
 | ID: U50387016
 | Initial Build: 1/3/2013
-| Last Revised: 4/24/13
+| Last Revised: 4/26/13
 | Copyright of Todd Sukolsky
 |================================================================================
 | Description: This is the main program for the ATmega324PA, project theWeather.system.
@@ -19,6 +19,9 @@
 |	4/24: Changed functionality to a less speratic model. Now this guy will take readings, print,
 |		  go to sleep, and wake up after so many timer overflows. This is ideal. Now comes the time
 |		  to work on SPI with the ATtiny and I2C with the RTC.
+|	4/26: Added longer timeout to function, added UART receive technique and flags to get  certain things.
+|		  Added polling and flag set for query requests.
+|		  
 |================================================================================
 | *NOTES:
 \*******************************************************************************/
@@ -32,9 +35,10 @@
 #include <string.h>
 #include "stdtypes.h"
 #include "ATmega324PA.h"
+#include "myUart.h"
 
 /**************Baud defines******************/
-#define FOSC 8000000					//using internal 8Mhz crystal, no clock divide
+#define FOSC 1000000					//using internal 8Mhz crystal, no clock divide
 //#define FOSC 14747560					//using external 14.74756 MHz crystal with no clock divide (X2)
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD - 1			//declares baud rate
@@ -49,14 +53,16 @@
 /**************Debug LED Value**************/
 #define DEBUG_NUMBER 0xF8				//8'b11111000
 
-#define SLEEP_TIME 120					//should sleep for 10 seconds.
-	
+#define TIMEOUT		976*5				//5 second timeout with UART receptions.
+#define SLEEP_TIME  976*10				
+#define PULSE_TIME	488
+//one second is 976 clock ticks at 1MHz, so half a second is 488.	
 /* ------------------------------------------------------------ */
 /*				Global Variables								*/
 /* ------------------------------------------------------------ */
 
-BYTE sleepCnt=0;
-BOOL flagGoToSleep;
+WORD sleepCnt=0;
+BOOL flagGoToSleep, flagAllStats,flagHumidity, flagTemp, flagReceivePi;
 
 /* ------------------------------------------------------------ */
 /*				Forward Declarations							*/
@@ -65,13 +71,11 @@ BOOL flagGoToSleep;
 void DeviceInit (void);
 void AppInit (unsigned int ubrr); //initializes UART, LEDs, other things
 void Wait_ms(WORD delay);
-void PutUart0Ch(char ch);
-void Print0(char string[]);
 void ResetDebug();
-void GetTempADT();
-void GetHumidity();
-void GetTempTI(unsigned int ubrr1);
-void InitTimer0();
+double GetTempADT();
+double GetTempTherm();
+double GetHumidity();
+double GetTempTI(unsigned int ubrr1);
 void InitTimer2();
 
 /* ------------------------------------------------------------ */
@@ -79,22 +83,26 @@ void InitTimer2();
 /* ------------------------------------------------------------ */
 
 ISR(INT2_vect){
-	prtDebug = (1 << bnD2);	//should toggle amber LED on. 
+	cli();
+	prtDebug |= (1 << bnD1);	//should toggle amber LED on. 
 	Wait_ms(10);
-	//Clears interrupt vector
+	flagReceivePi=fTrue;
+	prtDebug &= ~(1 << bnD1);
+	sei();
 }
-
-
-ISR(TIMER0_OVF_vect){
-	static BYTE timerZeroCount=0;
-	timerZeroCount++;;
-	if (timerZeroCount >= 20) {pinDebug |= (1 << bnD0); timerZeroCount = 0;}		//should toggle amber LED and red to left of it
-}	
 
 //used for sleeping.
 ISR(TIMER2_OVF_vect){
-	static BYTE timerTwoCount=0;
-	if (timerTwoCount++ >=40) {pinDebug = (1 << bnD0); timerTwoCount = 0;}		//toggles green, left LED
+	cli();
+	static WORD timerTwoCount=0,receivePiTimer=0;
+	
+	if (timerTwoCount++ >=PULSE_TIME) {pinDebug = (1 << bnD0); timerTwoCount = 0;}		//toggles green, left LED. Should be on half second.
+	
+	//Receive Timeout	
+	if (flagReceivePi && receivePiTimer<TIMEOUT){receivePiTimer++;}								//no timteout yet
+	else if (flagReceivePi && receivePiTimer>=TIMEOUT){receivePiTimer=0;flagReceivePi=fFalse;}	//timeout
+	else {receivePiTimer=0;}																	//make sure this statys at 0 and is reset if a successful receive happens		
+	sei();
 }
 
 
@@ -103,39 +111,69 @@ int main(void)
 {	
 	DeviceInit();
 	AppInit(MYUBRR);
-	sei();
 	Wait_ms(40);
 	ResetDebug();	//clear LED's
-	//InitTimer0();
 	InitTimer2();
-
+	sei();
 	// main program loop
 	while (fTrue) {	
 		
-		//Get Humidity from HoneyWell sensor, located on ADC1
-		prtDebug |= (1 << bnD0);
-		GetHumidity();
-		Wait_ms(10);
-		ResetDebug();
-
+		//IF receiving from the Pi
+		if (flagReceivePi){
+			prtDebug |= (1 << bnD1);
+			ReceivePi();
+			prtDebug &= ~(1 << bnD1);
+		}
 		
-		//Get temp data from ADT7302 source, on SPI
-		prtDebug |= (1 << bnD1);
-		GetTempADT();
-		Wait_ms(10);
-		ResetDebug();
+		//If we are suposed to get something, go get it
+		if (flagAllStats || flagTemp || flagHumidity){
+			cli();
+			prtDebug |= (1 << bnD2);
+			char printString[40];
+			//If we are supposed to get a temperature, get the three temps
+			if (flagAllStats || flagTemp){
+				prtDebug |= (1 << bnD3);
+				double adtTemp=GetTempADT();
+				double tiTemp=GetTempTI(MYUBRR);
+				double thermTemp=GetTempTherm();
+				char adtStr[5],tiStr[5],thermStr[5];
+				dtostrf(adtTemp,0,2,adtStr);
+				dtostrf(thermTemp,0,2,thermStr);
+				dtostrf(tiTemp,0,2,tiStr);
+				strcpy(printString,"ADT");
+				strcat(printString,adtStr);
+				strcat(printString,"/TI");
+				strcat(printString,tiStr);
+				strcat(printString,"/TH");
+				strcat(printString,thermStr);
+				prtDebug &= ~(1 << bnD3);
+			}
+			//If we should be getting humidity, get it.
+			if (flagAllStats || flagHumidity){
+				prtDebug |= (1 << bnD4);
+				double humidity=GetHumidity();
+				char humidityStr[5];
+				dtostrf(humidity,0,2,humidityStr);
+				//If we are only getting humidity, copy the string into the buffer
+				if (!flagAllStats && !flagTemp){
+					strcpy(printString,"HU");
+					strcat(printString,humidityStr);
+				//If temp was gotten as well, append to the buffer	
+				} else {
+					strcat(printString,"/HU");
+					strcat(printString,humidityStr);
+				}//end if only flagHumidity
+				prtDebug &= ~(1 << bnD4);
+			}//end if humidity needs to be gotten
+			
+			//Add end of line, X
+			strcat(printString,"X\0");
+			//Print the string to the RasberryPi
+			Print0(printString);
+			prtDebug &= ~(1 << bnD2);
+			sei();
+		}
 		
-		//Get temperature data from TI source on SPI/USART0
-		prtDebug |= (1 << bnD2);
-		GetTempTI(MYUBRR);
-		Wait_ms(10);
-		ResetDebug();
-		
-		//Get temperature data from Thermistor on ADC2
-		prtDebug |= (1 << bnD3);
-		GetTempTherm();
-		Wait_ms(10);
-						
 		//Power save. Should sleep for ~10 seconds
 		Print0(" Going to sleep... ");
 		ResetDebug();						//Clear the LED's
@@ -143,7 +181,7 @@ int main(void)
 		sleepCnt=0;
 		SMCR = (1 << SM1)|(1 << SM0);
 		SMCR |= (1 << SE);
-		while (sleepCnt<SLEEP_TIME){
+		while (sleepCnt<SLEEP_TIME && !flagReceivePi){
 			asm volatile("SLEEP");
 			sleepCnt++;
 		}		
@@ -205,30 +243,20 @@ void AppInit(unsigned int ubrr)
 	prtDebug |= (1 << bnMFP)|(1 << bnSDA)|(1 << bnSCL);
 */
 	//Initialize power outputs
-	PRR0 |= (1 << PRTWI)|(1 << PRTIM2)|(1 << PRTIM0)|(1 << PRUSART1)|(1 << PRTIM1)|(1 << PRADC)|(1 << PRSPI); //Turn EVERYTHING off initially
+	PRR0 |= (1 << PRTWI)|(1 << PRTIM2)|(1 << PRTIM1)|(1 << PRTIM0)|(1 << PRUSART1)|(1 << PRADC)|(1 << PRSPI); //Turn EVERYTHING off initially
 
 	//set up interrupts for waking device up through INT2
 	EICRA |= (1 << ISC21)|(1 << ISC20);		//rising edge of INT2 triggers asynchronous interrupt
 	EIMSK  = (1 << INT2);					//enables interrupts on INT2 as long as global interrupt is set.
 	
+	//Initialize Booleans
 	flagGoToSleep=fFalse;
+	flagAllStats=fFalse;
+	flagHumidity=fFalse;
+	flagTemp=fFalse;
+	flagReceivePi=fFalse;
 }
 
-/**********************************************************************************************************************************/
-void InitTimer0(){
-	//Disable glbal interrupts
-	cli();
-	//Enable TIMER0 power
-	PRR0 &= ~(1 << PRTIM0);
-	Wait_ms(1);
-	//Set up regiterst
-	TCCR0B = (1 << CS02)|(1 << CS00);			//prescaler of 256 from clkIO
-	TIFR0 = (1 << TOV0);						//Clear TOV0/ any pending interrupts
-	TIMSK0 = (1 << TOIE0);						//Enable overflow interrupt service routine
-	//Enable global interrupts
-	sei();
-}	
-	
 /**********************************************************************************************************************************/
 void InitTimer2(){
 	//Disable global interrupts
@@ -246,10 +274,9 @@ void InitTimer2(){
 }
 /**********************************************************************************************************************************/
 
-void GetTempTI(unsigned int ubrr)
+double GetTempTI(unsigned int ubrr)
 {
 	WORD tempDataTI = 0;
-	char tempStringTI[8];
 	
 	//Bring the USART1 back online, reset regitser
 	PRR0 &= ~(1 << PRUSART1);	//give it power
@@ -300,22 +327,18 @@ void GetTempTI(unsigned int ubrr)
 	PRR0 |= (1 << PRUSART1);	//turns off power to USART1
 	
 	//Manipulate tempDataTI
-	float dataTI = (tempDataTI/4)*ON_CHIP_CONVERTER*1.8 + 32;			//Div 4 shifts right 2 bits; Converts into celcius, then into Fahrenheit
-	dtostrf(dataTI, 5, 2, tempStringTI);
-	tempStringTI[6] = '.';
-	tempStringTI[7] = '\0';
-	Print0(" TI temp= ");
-	Print0(tempStringTI);	
+	double dataTI = (tempDataTI/4)*ON_CHIP_CONVERTER*1.8 + 32;			//Div 4 shifts right 2 bits; Converts into celcius, then into Fahrenheit
+
+	return dataTI;	
 		
 }
 
 /**********************************************************************************************************************************/
-void GetTempTherm()
+double  GetTempTherm()
 {
 	//Function variables
 	WORD tempDataTherm = 0;
-	float dataTherm = 0;
-	char stringTherm[10];
+	double dataTherm = 0;
 	
 	//Turn ADC on
 	PRR0 &= ~(1 << PRADC);
@@ -343,13 +366,6 @@ void GetTempTherm()
 	
 	//Get offset of temperature from maximum ADC reading and offset to 0 for our linear progression
 	tempDataTherm = 1023 - tempDataTherm - THERM_OFFSET;					//For 0->40 degree C	
-	#ifdef DEBUG_THERM
-	char shibby[10];
-	utoa(tempDataTherm,shibby,10);
-	shibby[9] = '\0';
-	Print0(" RAW Therm ADC= ");
-	Print0(shibby);
-	#endif
 	
 	//Multiply by conversion factor
 	dataTherm = tempDataTherm/THERM_CONVERTER;	//now in celsius
@@ -357,23 +373,16 @@ void GetTempTherm()
 	//Convert to Fahrenheit; F = C*(9/5) * 32; 74.4 correlates to 23.555 Celsius
 	dataTherm = dataTherm*1.8 + 32;	
 	
-	//Convert into a string
-	dtostrf(dataTherm,5,2,stringTherm);
-	stringTherm[8] = '.';
-	stringTherm[9] = '\0';
-	
-	//Print temperature
-	Print0(" Therm Temp= ");
-	Print0(stringTherm);
+	return dataTherm;
+
 		
 }
 
 /**********************************************************************************************************************************/	
-void GetHumidity()
+double GetHumidity()
 {
 	//Function variables
 	WORD humidityResult;
-	char humidityString[10];
 	
 	//Turn on power to ADC, turn on components
 	PRR0 &= ~(1 << PRADC);				//give ADC power again
@@ -399,24 +408,17 @@ void GetHumidity()
 	PRR0 |= (1 << PRADC);
 
 	//Get actual humidity
-	float humidityResultFloat = (float)humidityResult;
-	humidityResultFloat -= HUMIDITY_OFFSET;	//155 is 0 for a conversion, then divide the converting factor 
-	humidityResultFloat /= HUMIDITY_CONVERTER;	//get actual humidity	
+	double humidityResultDouble = (double)humidityResult;
+	humidityResultDouble -= HUMIDITY_OFFSET;	//155 is 0 for a conversion, then divide the converting factor 
+	humidityResultDouble /= HUMIDITY_CONVERTER;	//get actual humidity	
 	
-	//Print humidity to terminal
-	dtostrf(humidityResultFloat, 5, 2, humidityString);
-	humidityString[8] = '.';
-	humidityString[9] = '\0';
-	Print0(" Humidity: ");
-	Print0(humidityString);
+	return humidityResultDouble;
 }
-
 /**********************************************************************************************************************************/	
 
-void GetTempADT()
+double GetTempADT()
 {
 	WORD rawDataAD = 0;
-	char tempStringAD[8];
 	
 	//Turn SPI on
 	PRR0 &= ~(1 << PRSPI);
@@ -438,14 +440,9 @@ void GetTempADT()
 	Wait_ms(10);						//Give it time to settle
 	
 	//Convert number into Celsius, then Fahrenheit
-	float dataAD = rawDataAD*ON_CHIP_CONVERTER*1.8 + 32;
+	double dataAD = rawDataAD*ON_CHIP_CONVERTER*1.8 + 32;
 	
-	//Print string
-	dtostrf(dataAD, 5, 2, tempStringAD);
-	tempStringAD[6] = '.';
-	tempStringAD[7] = '\0';
-	Print0(" AD temp= ");
-	Print0(tempStringAD);
+	return dataAD;
 }
 
 /**********************************************************************************************************************************/	
@@ -469,23 +466,3 @@ void Wait_ms(WORD delay)
 }
 
 /**********************************************************************************************************************************/
-
-void PutUart0Ch(char ch)
-{
-	while (! (UCSR0A & (1 << UDRE0)));
-	UDR0 = ch;
-}
-
-/**********************************************************************************************************************************/
-void Print0(char string[])
-{	
-	BYTE i;
-	i = 0;
-
-	while (string[i]) {
-		PutUart0Ch(string[i]);  //send byte		
-		i += 1;
-	}
-}
-
-/************************************************************************/
